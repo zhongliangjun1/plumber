32fdb844c82250322bf4ae7c8b256675af75753b
liangjun.zhong <zhongliangjun1@gmail.com>
2014-11-11 02:13:01 +0800

    add BarrierDispatcherInterceptor

--- src/main/java/com/dianping/plumber/core/interceptors/BarrierDispatcherInterceptor.java
+++ src/main/java/com/dianping/plumber/core/interceptors/BarrierDispatcherInterceptor.java
@@ -2,7 +2,11 @@ package com.dianping.plumber.core.interceptors;
 
 import com.dianping.plumber.core.*;
 import com.dianping.plumber.utils.CollectionUtils;
-import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,16 +21,23 @@ public class BarrierDispatcherInterceptor implements Interceptor {
     @Override
     public ResultType intercept(InvocationContext invocation) throws Exception {
         String controllerName = invocation.getControllerName();
-        ApplicationContext applicationContext = invocation.getApplicationContext();
-        PlumberController controller = (PlumberController) applicationContext.getBean(controllerName);
         PlumberControllerDefinition controllerDefinition = PlumberWorkerDefinitionsRepo.getPlumberControllerDefinition(controllerName);
-        if ( CollectionUtils.isEmpty(controllerDefinition.getBarrierNames()) ) {
-            return invocation.invoke();
-        } else {
-            controllerDefinition.getBarrierNames();
+        if ( !CollectionUtils.isEmpty(controllerDefinition.getBarrierNames()) ) {
+            List<PlumberBarrierDefinition> barrierDefinitions = controllerDefinition.getBarrierDefinitions();
+            CountDownLatch barrierLatch = new CountDownLatch(barrierDefinitions.size());
+            Map<String, Object> paramsFromController = invocation.getParamsForPagelets();
+            ConcurrentHashMap<String,String> barrierRenderResults = new ConcurrentHashMap<String, String>();
+            for ( PlumberBarrierDefinition barrierDefinition : barrierDefinitions ) {
+                String barrierName = barrierDefinition.getName();
+                PlumberBarrier barrier = (PlumberBarrier) invocation.getApplicationContext().getBean(barrierName);
+                PlumberBarrierWorker barrierWorker = new PlumberBarrierWorker(barrierDefinition, paramsFromController,
+                        barrierLatch, barrier, barrierRenderResults);
+            }
+            barrierLatch.await();
+            ConcurrentHashMap<String, Object> modelForControllerView = invocation.getModelForControllerView();
+            modelForControllerView.putAll(barrierRenderResults);
         }
-
-        return null;
+        return invocation.invoke();
     }
 
 
