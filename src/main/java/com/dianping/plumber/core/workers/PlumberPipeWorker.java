e0f1e2a18569aedfd5ee0fb2e8bb3ed9d9c56d13
zhongliangjun1 <zhongliangjun1@gmail.com>
2015-05-23 22:58:40 +0800

    adjust PlumberPipeWorker

--- src/main/java/com/dianping/plumber/core/workers/PlumberPipeWorker.java
+++ src/main/java/com/dianping/plumber/core/workers/PlumberPipeWorker.java
@@ -1,43 +1,37 @@
 package com.dianping.plumber.core.workers;
 
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+
 import com.dianping.plumber.config.PlumberConfig;
 import com.dianping.plumber.core.*;
 import com.dianping.plumber.core.definitions.PlumberPipeDefinition;
-import com.dianping.plumber.core.workers.PlumberWorker;
 import com.dianping.plumber.exception.PlumberPipeTimeoutException;
 import com.dianping.plumber.utils.EnvUtils;
 import com.dianping.plumber.utils.StringUtils;
 import com.dianping.plumber.view.ViewRenderer;
-import org.apache.commons.lang.exception.ExceptionUtils;
-
-import java.util.Map;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
 
 /**
  * Created with IntelliJ IDEA.
  * Author: liangjun.zhong
  * Date: 14-11-11
  * Time: PM10:29
- * To change this template use File | Settings | File Templates.
  */
 public class PlumberPipeWorker extends PlumberWorker {
 
-    private final PlumberPagelet pipe;
+    private final PlumberPagelet              pipe;
     private final LinkedBlockingQueue<String> pipeRenderResultQueue;
-    private final boolean hasPriority;
-    private final ResultReturnedFlag resultReturnedFlag;
+    private final boolean                     hasPriority;
+    private final ResultReturnedFlag          resultReturnedFlag;
 
     public PlumberPipeWorker(PlumberPipeDefinition definition,
                              Map<String, Object> paramsFromRequest,
-                             Map<String, Object> paramsFromController,
-                             PlumberPagelet pipe,
+                             Map<String, Object> paramsFromController, PlumberPagelet pipe,
                              LinkedBlockingQueue<String> pipeRenderResultQueue,
-                             boolean hasPriority,
-                             ResultReturnedFlag resultReturnedFlag) {
+                             boolean hasPriority, ResultReturnedFlag resultReturnedFlag) {
         super(definition, paramsFromRequest, paramsFromController);
         this.pipe = pipe;
         this.pipeRenderResultQueue = pipeRenderResultQueue;
@@ -49,27 +43,28 @@ public class PlumberPipeWorker extends PlumberWorker {
     public void run() {
         String renderResult = PlumberGlobals.EMPTY_RENDER_RESULT;
         try {
-            ResultType resultType = pipe.execute(paramsFromRequest, paramsFromController, modelForView);
-            if ( resultType==ResultType.SUCCESS ) {
+            ResultType resultType = pipe.execute(paramsFromRequest, paramsFromController,
+                modelForView);
+            if (resultType == ResultType.SUCCESS) {
                 String name = definition.getName();
                 String viewSource = definition.getViewSource();
-                if ( EnvUtils.isDev() ) { // for refresh
+                if (EnvUtils.isDev()) { // for refresh
                     String viewPath = definition.getViewPath();
                     viewSource = PlumberWorkerDefinitionsRepo.getViewSourceLoader().load(viewPath);
                 }
                 ViewRenderer viewRenderer = PlumberWorkerDefinitionsRepo.getViewRenderer();
                 String result = viewRenderer.render(name, viewSource, modelForView);
-                if ( StringUtils.isNotEmpty(result) )
+                if (StringUtils.isNotEmpty(result))
                     renderResult = result;
             }
         } catch (Exception e) {
 
             logger.error("pipe " + definition.getName() + " execute failure", e);
 
-            if ( EnvUtils.isDev() ) {
+            if (EnvUtils.isDev()) {
                 try {
                     String result = ExceptionUtils.getFullStackTrace(e);
-                    if ( StringUtils.isNotEmpty(result) )
+                    if (StringUtils.isNotEmpty(result))
                         renderResult = result;
                 } catch (Exception e1) {
                     logger.error("can not get the exception full stack trace", e1);
@@ -78,35 +73,10 @@ public class PlumberPipeWorker extends PlumberWorker {
 
         } finally {
 
-            if ( flushLock==null ) {
-
+            if (!hasPriority) {
                 sendBackRenderResult(renderResult);
-
             } else {
-
-                PlumberPipeDefinition plumberPipeDefinition = (PlumberPipeDefinition) definition;
-                Integer pipeSeqLocation = plumberPipeDefinition.getSeqLocation();
-
-                flushLock.lock();
-                try {
-                    while ( pipeSeqLocation>=currentPipeSeqLocation.get() ) {
-                        if ( pipeSeqLocation==currentPipeSeqLocation.get() ) {
-                            sendBackRenderResult(renderResult);
-                            currentPipeSeqLocation.incrementAndGet();
-                            flushCondition.signalAll();
-                        } else {
-                            try {
-                                flushCondition.await();
-                            } catch (InterruptedException e) {
-                                logger.error("can not return the pipe " + definition.getName() + "'s render result", e);
-                                break;
-                            }
-                        }
-                    }
-                } finally {
-                    flushLock.unlock();
-                }
-
+                signalMonitor(renderResult);
             }
 
         }
@@ -114,23 +84,29 @@ public class PlumberPipeWorker extends PlumberWorker {
 
     private void sendBackRenderResult(String renderResult) {
 
-        if ( resultReturnedFlag.isReturned() ) {
-            logger.error("can not return the pipe " + definition.getName() + "'s render result", new PlumberPipeTimeoutException());
+        if (resultReturnedFlag.isReturned()) {
+            logger.error("can not return the pipe " + definition.getName() + "'s render result",
+                new PlumberPipeTimeoutException());
             return;
         }
 
         try {
-            boolean insertResult = pipeRenderResultQueue.offer(renderResult, PlumberConfig.getResponseTimeout(), TimeUnit.MILLISECONDS);
-            if ( !insertResult ) {
-                logger.error("can not return the pipe " + definition.getName() + "'s render result", new PlumberPipeTimeoutException());
+            boolean insertResult = pipeRenderResultQueue.offer(renderResult,
+                PlumberConfig.getResponseTimeout(), TimeUnit.MILLISECONDS);
+            if (!insertResult) {
+                logger.error(
+                    "can not return the pipe " + definition.getName() + "'s render result",
+                    new PlumberPipeTimeoutException());
             }
         } catch (InterruptedException e) {
-            logger.error("can not return the pipe " + definition.getName() + "'s render result", new PlumberPipeTimeoutException(e));
+            logger.error("can not return the pipe " + definition.getName() + "'s render result",
+                new PlumberPipeTimeoutException(e));
         }
 
     }
 
-
-
+    private void signalMonitor(String renderResult) {
+        // to do
+    }
 
 }
