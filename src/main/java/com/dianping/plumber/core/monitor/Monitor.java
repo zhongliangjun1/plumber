02021400cf09e50946149d9a44e0a25d052eccf1
zhongliangjun1 <zhongliangjun1@gmail.com>
2015-05-29 15:46:55 +0800

    add

--- src/main/java/com/dianping/plumber/core/monitor/Monitor.java
+++ src/main/java/com/dianping/plumber/core/monitor/Monitor.java
@@ -4,11 +4,17 @@
  */
 package com.dianping.plumber.core.monitor;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+
 import com.dianping.plumber.config.PlumberConfig;
+import com.dianping.plumber.utils.CollectionUtils;
 import com.dianping.plumber.utils.TimeUtils;
 
 /**
@@ -17,7 +23,24 @@ import com.dianping.plumber.utils.TimeUtils;
  */
 public abstract class Monitor {
 
+    private static final Logger                            logger           = Logger
+                                                                                .getLogger(Monitor.class);
     private static final LinkedBlockingQueue<MonitorEvent> eventsCacheQueue = new LinkedBlockingQueue<MonitorEvent>();
+    private static final List<MonitorEvent>                monitorEvents    = new LinkedList<MonitorEvent>();
+
+    static {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    add();
+                    monitor();
+                    clear();
+                }
+            }
+        });
+    }
 
     public static void listen(MonitorEvent event) throws InterruptedException {
         eventsCacheQueue.offer(event,
@@ -34,6 +57,34 @@ public abstract class Monitor {
                 break;
             }
         }
+    }
+
+    private static void add() {
+        MonitorEvent newMonitorEvent;
+
+        if (CollectionUtils.isEmpty(monitorEvents)) {
+            try {
+                newMonitorEvent = eventsCacheQueue.take();
+            } catch (InterruptedException e) {
+                logger.error("take monitor event from eventsCacheQueue error", e);
+                newMonitorEvent = null;
+            }
+        } else {
+            newMonitorEvent = eventsCacheQueue.poll();
+        }
+
+        if (newMonitorEvent != null) {
+            monitorEvents.add(newMonitorEvent);
+            add();
+        }
+    }
+
+    private static void monitor() {
+
+    }
+
+    private static void clear() {
+
     }
 
 }
